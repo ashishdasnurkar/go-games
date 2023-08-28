34b690d9f573811f3d32b6e2b9f841fc19b3a6cc
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2023-08-28 21:01:36 +1000

    print game state

--- guesstheword/main.go
+++ guesstheword/main.go
@@ -23,10 +23,25 @@ var dictionary = []string{
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	targetWord := getRandomWord()
+	guessedLetters := map[rune]bool{}
+	guessedLetters[rune(targetWord[0])] = true
+	guessedLetters[rune(targetWord[len(targetWord)-1])] = true
+	printGameState(targetWord, guessedLetters)
 	fmt.Println(targetWord)
 }
 
 func getRandomWord() string {
 	targetWord := dictionary[rand.Intn(len(dictionary))]
 	return targetWord
+}
+
+func printGameState(targetWord string, guessedLeters map[rune]bool) {
+	for _, ch := range targetWord {
+		if guessedLeters[ch] == true {
+			fmt.Printf("%c", ch)
+		} else {
+			fmt.Print(" _ ")
+		}
+	}
+	fmt.Println()
 }
